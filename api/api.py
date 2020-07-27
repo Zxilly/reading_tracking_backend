d7a2fe8853134896dd88714579f9dc858cc3a035
Zxilly <zhouxinyu1001@gmail.com>
2020-07-27 18:46:23 +0800

    finished

--- api/api.py
+++ api/api.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel
-from typing import Optional,List
-
-from fastapi import FastAPI,Body
-from fastapi.encoders import jsonable_encoder
-from fastapi.middleware.cors import CORSMiddleware
+import json
+from typing import Optional, List
 
 import uvicorn
-import json
+from fastapi import FastAPI, Body
+from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+from book import BookObject
 
 app = FastAPI()
 
@@ -18,31 +19,36 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class Book(BaseModel):
     progress: int
     isbn: str
     tip: str
+    data: Optional[dict] = None
 
-def name_get(user:str):
-    return '../data/user/'+user+'.json'
+
+def name_get(user: str):
+    return '../data/user/' + user + '.json'
 
 
 @app.get('/api')
-async def output(user:str):
+async def output(user: str):
     try:
-        with open(name_get(user),'r+') as f:
-            return {'code':1,'data':json.loads(f.read())}
+        with open(name_get(user), 'r+') as f:
+            return {'code': 1, 'data': json.loads(f.read())}
     except:
-        return {'code':0,'msg':'Can not find any record. Please add your tracking first.'}
+        return {'code': 0, 'msg': 'Can not find any record. Please add your tracking first.'}
 
 
 @app.post('/api')
-async def input(user:str,books:Optional[List[Book]]=Body(...,embed=True)):
-    with open(name_get(user),'w+') as f:
+async def input(user: str, books: Optional[List[Book]] = Body(..., embed=True)):
+    for book in books:
+        BookInfo = BookObject(book.isbn)
+        book.data = BookInfo.info_dict
+    with open(name_get(user), 'w+') as f:
         f.write(json.dumps(jsonable_encoder(books)))
-    return {'code':1,'msg':'Update success.'}
-
+    return {'code': 1, 'msg': 'Update success.'}
 
 
 if __name__ == '__main__':
-    uvicorn.run(app='api:app',host='0.0.0.0',port=4000,debug=False)
+    uvicorn.run(app='api:app', host='0.0.0.0', port=4000, debug=False)
--- api/book.py
+++ api/book.py
@@ -1,14 +1,48 @@
-import requests
+import base64
 import json
+from functools import reduce
 
-def get_book(isbn:str):
-    url = "https://api.douban.com/v2/book/isbn/"
-    api_key = "0b2bdeda43b5688921839c8ecb20399b"
+import requests
 
-    def get_cache():
-        with open('../data/books/{}'.format(isbn),'r+') as f:
-            ...
 
-    def add_cache():
-        req = json.loads(requests.get(url+isbn,params={"apikey":api_key}).content)
+class BookObject(object):
+    def __init__(self, isbn: str):
+        self.isbn = isbn
+        self.url = "https://api.douban.com/v2/book/isbn/"
+        self.api_key = "0b2bdeda43b5688921839c8ecb20399b"
+        try:
+            self.get_cache()
+        except:
+            self.add_cache()
+        self.info_dict = {
+            'title': self.title,
+            'sub_title': self.sub_title,
+            'pic_base64': self.pic_base64,
+            'page_total': self.page_total,
+            'author_str': self.author_str
+        }
 
+    def get_cache(self):
+        with open('../data/books/{}'.format(self.isbn), 'r+') as f:
+            data = json.loads(f.read())
+            self.title = data['title']
+            self.sub_title = data['sub_title']
+            self.pic_base64 = data['pic_base64']
+            self.page_total = data['page_total']
+            self.author_str = data['author_str']
+
+    def add_cache(self):
+        data = json.loads(requests.get(self.url + self.isbn, params={"apikey": self.api_key}).content)
+        self.title = data['title']
+        self.sub_title = data['subtitle']
+        self.pic_base64 = 'data:image/jpg;base64,' + base64.b64encode(requests.get(data['images']['medium'].content))
+        self.page_total = data['pages']
+        self.author_str = reduce(lambda str, list_one: str + '; ' + list_one, data['author'])
+        with open('../data/books/{}'.format(self.isbn), 'w+') as f:
+            f.write(json.dumps({
+                'title': self.title,
+                'sub_title': self.sub_title,
+                'pic_base64': self.pic_base64,
+                'page_total': self.page_total,
+                'author_str': self.author_str
+            }))
